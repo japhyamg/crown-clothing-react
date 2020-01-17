7e72623151a4791e0b1a8d94e3af2fdab0ab0b39
Japhet Okparah <jalleto4rel@gmail.com>
2020-01-17 02:15:04 +0100

    finished up the signin component with signInWithEmailAndPassword method

--- src/components/signin/signin.component.js
+++ src/components/signin/signin.component.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import "./signin.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends Component {
   constructor() {
@@ -15,13 +15,23 @@ class SignIn extends Component {
     };
   }
 
-  handleSubmit = e => {
+  handleSubmit = async e => {
     e.preventDefault();
+    const { email, password } = this.state;
 
-    this.setState({
-      email: "",
-      password: ""
-    });
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({
+        email: "",
+        password: ""
+      });
+    } catch (error) {
+      console.log(error);
+      this.setState({
+        email: "",
+        password: ""
+      });
+    }
   };
 
   handleChange = e => {
--- src/components/signup/signup.component.js
+++ src/components/signup/signup.component.js
@@ -35,7 +35,10 @@ class SignUp extends React.Component {
     }
 
     try {
-      const user = await auth.createUserWithEmailAndPassword(email, password);
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
       await createUserProfileDocument(user, { displayName });
 
       this.setState({
